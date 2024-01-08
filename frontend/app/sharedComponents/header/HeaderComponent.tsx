bd7b5e3e4b9fd314782fdffff8a071ad2634be75
shimmieblum <shimmieblum@gmail.com>
2024-01-08 17:44:24 +0000

    added navigation into menu

--- frontend/app/sharedComponents/header/HeaderComponent.tsx
+++ frontend/app/sharedComponents/header/HeaderComponent.tsx
@@ -1,20 +1,55 @@
-import {AppBar, Box, IconButton, Toolbar, Button, Typography} from "@mui/material";
+import {AppBar, Box, IconButton, Toolbar, Button, Typography, Menu, MenuItem} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu'
 import Image from 'next/image';
 import WhiteLogo from './WhiteLogo.png';
+import {useState, MouseEvent, MouseEventHandler} from "react";
+import {useRouter} from "next/navigation";
 
 export const HeaderComponent = (props: {}) => {
-    
-  return <AppBar position={'static'}>
-    <Toolbar sx={{
-      backgroundColor: 'black',
-      borderBottom: 1,
-      borderBottomColor: 'white',
-    }}
+  const [showMenu, setShowMenu] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const router = useRouter();
+  const handleMenuOpen = (e: MouseEvent<HTMLElement>) => {
+    setShowMenu(true);
+    setAnchorEl(e.currentTarget);
+  }
+  
+  const handleMenuClose = () => {
+    setShowMenu(false);
+    setAnchorEl(null);
+  }
+  
+   const NavMenu = () => (
+    <Menu
+      open={showMenu}
+      anchorEl={anchorEl}
+      keepMounted
+      onClose={handleMenuClose}
     >
-      <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+      <MenuItem onClick={() => router.push('/sauces')}>Sauces</MenuItem>
+      <MenuItem onClick={() => router.push('/create-new-sauce')}>Create New Sauce</MenuItem>
+    </Menu>
+  );
+  
+  return <AppBar position={'static'}>
+    <Toolbar 
+      sx={{
+        backgroundColor: 'black',
+        borderBottom: 1,
+        borderBottomColor: 'white',
+      }}
+    >
+      <IconButton 
+        size='large'
+        edge='start'
+        color='inherit'
+        aria-label='menu'
+        sx={{ mr: 2 }}
+        onClick={handleMenuOpen}
+      >
           <MenuIcon />
       </IconButton>
+      <NavMenu/>
       <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
         Sauces Database
       </Typography>
