1529fc57d0c637fc1a13c1242aa98814c2d6c987
shimmieblum <shimmieblum@gmail.com>
2024-01-11 18:36:32 +0000

    moved form to shared-components

--- frontend/app/create-new-sauce/newSauceForm.tsx
+++ frontend/app/create-new-sauce/newSauceForm.tsx
@@ -1,16 +1,14 @@
 import React, {FormEvent, useState} from "react";
-import {Box, Button, Card, Paper, TextField, Typography} from "@mui/material";
-import {makeStyles} from '@mui/styles';
+import {Box, Button, Typography} from "@mui/material";
 import {FermentationRequest, SauceRequest} from "../../models/SauceRequestSchema";
-import {useCreateSauce, useDeleteSauce} from "../../hooks/useSaucesApi";
-import {IngredientsInput} from "./inputs/IngredientsInput";
+import {useCreateSauce} from "../../hooks/useSaucesApi";
 import {Ingredient} from "../../models/IngredientSchema";
-import {TextFieldInput, NumberFieldInput} from "./inputs/FieldInputs";
 import {ResponseSnackBars} from "../sharedComponents/ResponseSnackBars";
-import {BuildTwoTone} from "@mui/icons-material";
 import {useRouter} from "next/navigation";
+import {makeStyles} from "@mui/styles";
+import {SauceForm} from "../sharedComponents/SauceForm/SauceForm";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   submitSuccessBox: {
     margin: "auto",
     width: '50%',
@@ -21,48 +19,25 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  form: {
-    margin: "auto",
-    width: '50%',
-    maxWidth: '90%',
-    minWidth: 'fit-content',
-    display: 'flex', 
-    flexDirection: 'column',
-    alignItems: 'center', 
-    justifyContent: 'center',
-    background: 'white',
-    marginTop: 10,
-    padding: 50,
-    borderRadius: 50,
-  },
-  FermentationBox: {
-    marginTop: 5,
-    width: "90%",
-    margin: 'auto',
-    border: '1px solid lightgrey',
-    borderRadius: '5px',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  text: {
-    padding: '10px',
-  },
   button: {
     margin: 5,
   },
-}));
+}))
 
 export const NewSauceForm = () => {
   const classes = useStyles();
+  const [createSuccess, setCreateSuccess] = useState<boolean | undefined>()
+  const [createFailed, setCreateFailed] = useState<boolean | undefined>()
+  const [isFormSubmitted, setFormSubmitted] = useState(false);
+
   const [fermentation, setFermentation] = useState<FermentationRequest>();
   const [fermentationPercentage, setFermentationPercentage] = useState(0);
   const [name, setName] = useState('');
   const [notes, setNotes] = useState('');
   const [sauceId, setSauceId] = useState('');
   const [nonFermentedIngredients, setNonFermentedIngredients] = useState<Ingredient[]>([{ingredient: '', percentage: 0}]);
-  const [createSuccess, setCreateSuccess] = useState<boolean | undefined>()
-  const [createFailed, setCreateFailed] = useState<boolean | undefined>()
-  const [isFormSubmitted, setFormSubmitted] = useState(false);
+
+  
   const router = useRouter();
   const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
     if(!fermentation){
@@ -96,6 +71,16 @@ export const NewSauceForm = () => {
     router.push(`/sauces/${sauceId}`)
   }
   
+  const SubmitButton = (
+    <Button
+    className={classes.button}
+    variant='contained'
+    color='primary'
+    type='submit'
+  >
+    Create Sauce
+  </Button>);
+  
   const FormSubmitted = () => {
     return (
     <Box className={classes.submitSuccessBox} component='div'>
@@ -123,29 +108,17 @@ export const NewSauceForm = () => {
     <>
       {isFormSubmitted 
         ? <FormSubmitted /> 
-        : <Box className={classes.form} component='form' onSubmit={handleSubmit}>
-          <Typography margin='20px' variant='h4'>
-            CREATE NEW SAUCE
-          </Typography>
-          <TextFieldInput title='name' setValueFn={setName} autoFocus/>
-          <FermentationRecipeInput 
-            setFermentationFn={setFermentation}
-          />
-          <NonFermentedIngredientsInput 
-            setIngredients={setNonFermentedIngredients} 
-            ingredients={nonFermentedIngredients} 
-            setFermentedPercentage={setFermentationPercentage}
-          />
-          <TextFieldInput multiline required={false} title={'notes'} setValueFn={setNotes}/>
-          <Button 
-            className={classes.button} 
-            variant='contained' 
-            color='primary' 
-            type='submit'
-          >
-            Create Sauce
-          </Button>
-        </Box>
+        : <Box component='form' onSubmit={handleSubmit}>
+            <SauceForm 
+              title='CREATE NEW SAUCE'
+              name={{get: name, set: setName}} 
+              notes={{get: notes, set: setNotes}} 
+              fermentationPercentage={{get:fermentationPercentage, set:setFermentationPercentage}}
+              fermentation={{get:fermentation, set:setFermentation}} 
+              nonFermentedIngredients={{get:nonFermentedIngredients, set:setNonFermentedIngredients}}
+              buttons={SubmitButton}
+            />
+          </Box>
       }
       <ResponseSnackBars
         isGood={createSuccess}
@@ -157,48 +130,3 @@ export const NewSauceForm = () => {
     </>
   );
 }
-
-const FermentationRecipeInput = (props: {
-  setFermentationFn: (f:FermentationRequest) => void,
-}) => {
-  const classes = useStyles();
-  const [ingredients, setIngredients] = useState<Ingredient[]>([{ingredient: '', percentage: 0}]);
-  const [lengthInDays, setLengthInDays] = useState(0);
-  const handleChange = () => {
-    props.setFermentationFn({
-      ingredients: ingredients,
-      lengthInDays: lengthInDays
-    })
-  };
-
-  return(
-    <Box className={classes.FermentationBox} component='div' onChange={handleChange}>
-      <Typography className={classes.text} variant='body1'>
-        Fermentation Recipe
-      </Typography>
-      <IngredientsInput title='Ingredients' entries={ingredients} setEntries={setIngredients}/>
-      <NumberFieldInput title='Length (days)' setValueFn={setLengthInDays}/>
-  </Box>);
-}
-
-
-
-const NonFermentedIngredientsInput = ({ingredients, setIngredients, setFermentedPercentage} : {
-  ingredients: Ingredient[],
-  setIngredients: (f:Ingredient[]) => void,
-  setFermentedPercentage: (n:number) => void,
-}) => {
-  const classes = useStyles();
-  
-  return(
-    <Box className={classes.FermentationBox} component='div'>
-      <Typography className={classes.text} variant='body1'>
-        Sauce Recipe
-      </Typography>
-      <NumberFieldInput title='Fermentation Percentage' setValueFn={setFermentedPercentage} />
-      <IngredientsInput title='Ingredients' entries={ingredients} setEntries={setIngredients}/>
-    </Box>);
-}
-
-
-
