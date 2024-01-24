1d1ff75f0f22b3e1a96c03bf3f768ad5db2f9a1c
shimmieblum <shimmieblum@gmail.com>
2024-01-24 16:13:28 +0000

    renamed

--- backend/src/Sauces.Api/Models/IngredientModel.cs
+++ backend/src/Sauces.Api/Models/IngredientModel.cs
@@ -2,11 +2,12 @@ using System.Text.Json.Serialization;
 
 namespace Sauces.Api.Models;
 
-public class IngredientsModel
+public class IngredientModel
 {
     [JsonPropertyName("ingredient")]
     public string Ingredient { get; set; }
     
     [JsonPropertyName("percentage")]
     public int Percentage { get; set; }
+
 }
