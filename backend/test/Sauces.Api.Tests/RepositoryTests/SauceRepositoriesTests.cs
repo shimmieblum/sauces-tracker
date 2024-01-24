dab8e607c12fc915d460be72404f451a9381f658
shimmieblum <shimmieblum@gmail.com>
2024-01-24 16:20:28 +0000

    test updates

--- backend/test/Sauces.Api.Tests/RepositoryTests/SauceRepositoriesTests.cs
+++ backend/test/Sauces.Api.Tests/RepositoryTests/SauceRepositoriesTests.cs
@@ -2,8 +2,9 @@ using FluentAssertions;
 using Sauces.Api.Models;
 using Sauces.Api.Repositories;
 using Sauces.Api.Tests.FixturesAndUtils;
+using Sauces.Api.Tests.FixturesAndUtils.Builders;
 
-namespace Sauces.Api.Tests;
+namespace Sauces.Api.Tests.RepositoryTests;
 
 [TestFixture]
 public class SauceRepositoriesTests
@@ -21,7 +22,7 @@ public class SauceRepositoriesTests
     [Test]
     public async Task GetSauce_RetrievesSauceWithAllProperties_WhenItExists()
     {
-        var expectedSauce = _fixture.Sauce1;
+        var expectedSauce = _fixture.Sauce;
         var sut = GetSut();
         var sauce = await sut.GetAsync(expectedSauce.Id);
         sauce.Should().BeEquivalentTo(expectedSauce);
@@ -38,7 +39,7 @@ public class SauceRepositoriesTests
     [Test]
     public async Task CreateSauce_Succeeds()
     {
-        var recipe = _fixture.Recipe1;
+        var recipe = _fixture.Recipe;
         var request = new SauceRequestBuilder().FromDefault().WithRecipeId(recipe.Id).Build();
         var sut = GetSut();
         var sauceId = await sut.CreateAsync(request);
@@ -54,7 +55,7 @@ public class SauceRepositoriesTests
         sauce?.Fermentation.FermentationRecipe.Should().BeEquivalentTo(recipe);
         sauce?.Notes.Should().Be(request.Notes);
         sauce?.NonFermentedIngredients
-            .Select(i => new IngredientsModel
+            .Select(i => new IngredientModel
             {
                 Ingredient = i.Ingredient.Name,
                 Percentage = i.Percentage
@@ -63,9 +64,92 @@ public class SauceRepositoriesTests
     }
 
     [Test]
+    public async Task UpdateSauce_NewName_JustChangesName()
+    {
+        var ogSauce = _fixture.Sauce;
+        var newName = "new name";
+        var updateSauceRequest = new SauceUpdateRequestBuilder().FromSauce(ogSauce).WithName(newName).Build();
+
+        var sut = GetSut();
+        await sut.UpdateAsync(ogSauce.Id, updateSauceRequest);
+        var updatedSauce = await sut.GetAsync(ogSauce.Id); 
+        updatedSauce.Should().NotBeNull();
+        if (updatedSauce is null)
+        {
+            return;
+        }
+        updatedSauce.Name.Should().Be(newName);
+        updatedSauce.LastUpdated.Should().BeAfter(ogSauce.LastUpdated);
+        updatedSauce.Created.Should().Be(ogSauce.Created);
+        updatedSauce.Fermentation.Should().BeEquivalentTo(ogSauce.Fermentation);
+        updatedSauce.Notes.Should().BeEquivalentTo(ogSauce.Notes);
+        updatedSauce.Id.Should().Be(ogSauce.Id);
+    }
+
+    [Test]
+    public async Task UpdateSauce_NoChanges_DoesntChangeUpdatedAt()
+    {
+        var ogSauce = _fixture.Sauce;
+        var updateRequest = new SauceUpdateRequestBuilder().FromSauce(ogSauce).Build();
+        var updatedSauce = await GetSut().UpdateAsync(ogSauce.Id, updateRequest);
+        updatedSauce.Should().NotBeNull();
+        updatedSauce?.LastUpdated.Should().Be(ogSauce.LastUpdated);
+    }
+
+    [Test]
+    public async Task UpdateSauce_WithNewSauce_DoesntThrow()
+    {
+        var ogSauce = _fixture.Sauce;
+        var recipeRequest = new FermentationRecipeUpdateRequestBuilder()
+            .WithId(_fixture.RecipeId)
+            .WithIngredients(new IngredientModel { Ingredient = Guid.NewGuid().ToString(), Percentage = 100 })
+            .WithLengthInDays(_fixture.Recipe.LengthInDays + 10)
+            .Build();
+        var sauceRequest = new SauceUpdateRequestBuilder()
+            .WithName(ogSauce.Name + " new")
+            .WithFermentationPercentage(ogSauce.Fermentation.Percentage + 10)
+            .WithNotes(ogSauce.Notes + "extra")
+            .WithNonFermentedIngredients(new IngredientModel
+                { Ingredient = Guid.NewGuid().ToString(), Percentage = 100 })
+            .WithFermentationRecipe(recipeRequest)
+            .Build();
+        var updateSauce = async () => await GetSut().UpdateAsync(_fixture.SauceId, sauceRequest);
+        await updateSauce.Should().NotThrowAsync();
+    }
+    
+    [Test]
+    public async Task UpdateSauce_ChangedRecipe_UpdatesSauceAndRecipe()
+    {
+        var ogSauce = _fixture.Sauce;
+        var newIngredient = new IngredientModel { Ingredient = Guid.NewGuid().ToString(), Percentage = 100 };
+
+        var fermentationRecipe = new FermentationRecipeUpdateRequestBuilder()
+            .FromFermentationRecipe(ogSauce.Fermentation.FermentationRecipe)
+            .WithIngredients(newIngredient)
+            .Build();
+        var updateRequest = new SauceUpdateRequestBuilder()
+            .FromSauce(ogSauce)
+            .WithFermentationRecipe(fermentationRecipe)
+            .Build();
+
+        var updatedSauce = await GetSut().UpdateAsync(ogSauce.Id, updateRequest);
+        updatedSauce.Should().NotBeNull();
+        if(updatedSauce is null) return;
+        updatedSauce.Name.Should().Be(ogSauce.Name);
+        updatedSauce.LastUpdated.Should().BeAfter(ogSauce.LastUpdated);
+        updatedSauce.Fermentation.FermentationRecipe.Ingredients.Should().ContainSingle().And.AllSatisfy(i =>
+        {
+            i.Ingredient.Name.Should().Be(newIngredient.Ingredient);
+            i.Percentage.Should().Be(newIngredient.Percentage);
+        });
+        updatedSauce.Fermentation.FermentationRecipe.LastUpdate.Should()
+            .BeAfter(ogSauce.Fermentation.FermentationRecipe.LastUpdate);
+    }
+    
+    [Test]
     public async Task DeleteSauce_RemovesSauceFromDb()
     {
-        var id = _fixture.Sauce1.Id;
+        var id = _fixture.Sauce.Id;
         var sut = GetSut();
         await sut.DeleteAsync(id);
         var sauce = await sut.GetAsync(id);
